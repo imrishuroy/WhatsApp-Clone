456e3a51b0489e0a013adb580369a3668e6a1eab
Rishu Kumar <rishukumar.prince@gmail.com>
2020-07-06 00:23:19 +0530

    InIt

---
+++ ParseStarterProject/src/main/java/com/parse/starter/ChatActivity.java
@@ -0,0 +1,134 @@
+package com.parse.starter;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class ChatActivity extends AppCompatActivity {
+
+    String activeUser = "";
+
+    ArrayList<String> messages = new ArrayList<>();
+
+    ArrayAdapter arrayAdapter;
+
+
+
+    public void sendChat(View view){
+
+        final EditText chatEditText = findViewById(R.id.chatEditText);
+
+        ParseObject message = new ParseObject("Message");  // new parse class
+
+        final  String messageContent = chatEditText.getText().toString();
+
+        message.put("sender", ParseUser.getCurrentUser().getUsername());
+        message.put("recipient",activeUser);
+        message.put("message",messageContent);
+
+        chatEditText.setText("");
+
+        message.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+
+                if(e == null){
+
+                   // Toast.makeText(ChatActivity.this,"Message Sent",Toast.LENGTH_SHORT).show();
+
+                    messages.add(messageContent);
+
+                    arrayAdapter.notifyDataSetChanged();
+                }
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+
+
+        Intent intent = getIntent();
+
+        activeUser = intent.getStringExtra("username");
+
+        Log.i("Info", activeUser);
+
+        setTitle("Chat With " + activeUser);
+
+        ListView chatListView = findViewById(R.id.chatListView);
+
+        arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,messages);
+
+
+        chatListView.setAdapter(arrayAdapter);
+
+        ParseQuery<ParseObject> query1 = new ParseQuery<ParseObject>("Message");
+
+        query1.whereEqualTo("sender",ParseUser.getCurrentUser().getUsername());
+        query1.whereEqualTo("recipient",activeUser);
+
+        ParseQuery<ParseObject> query2 = new ParseQuery<ParseObject>("Message");
+
+        query2.whereEqualTo("recipient",ParseUser.getCurrentUser().getUsername());
+        query2.whereEqualTo("sender",activeUser);
+
+        List<ParseQuery<ParseObject>> queries = new ArrayList<ParseQuery<ParseObject>>();
+
+        queries.add(query1);
+        queries.add(query2);
+
+        ParseQuery<ParseObject> query = ParseQuery.or(queries);
+        query1.orderByAscending("createdAt");
+
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+
+                if(e == null){
+                    if(objects.size() > 0){
+
+                        for(ParseObject message : objects){
+
+                            String messageContent = message.getString("message");
+
+                            if(! message.getString("sender").equals(ParseUser.getCurrentUser().getUsername())){
+
+                                messageContent = "> " + messageContent;
+                            }
+
+                            Log.i("Info", messageContent);
+
+                            messages.add(messageContent);
+                        }
+
+                        arrayAdapter.notifyDataSetChanged();
+                    }
+                }
+
+            }
+        });
+
+    }
+}
---
+++ ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -0,0 +1,149 @@
+/*
+ * Copyright (c) 2015-present, Parse, LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+package com.parse.starter;
+import android.app.Notification;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseAnalytics;
+import com.parse.ParseAnonymousUtils;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+import com.parse.SignUpCallback;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+  Boolean loginModeActive = false;
+
+  public  void redirectIfLoggedIn(){
+    if(ParseUser.getCurrentUser() != null){
+
+      Intent intent = new Intent(getApplicationContext(),UserListActivity.class);
+      startActivity(intent);
+
+    }
+  }
+
+  public  void  toggleLoginMode(View view){
+
+    Button loginSignupButton = findViewById(R.id.loginSignupButton);
+
+    TextView toggleLoginModeTextView = findViewById(R.id.toggleLoginModeTextView);
+
+    if(loginModeActive){
+      loginModeActive = false;
+      loginSignupButton.setText("Sign Up");
+      toggleLoginModeTextView.setText("Or, Log In");
+
+
+    }else {
+      loginModeActive = true;
+      loginSignupButton.setText("Log In");
+      toggleLoginModeTextView.setText("Or, SignUp");
+    }
+  }
+
+  public void signupLogin(View view) {
+
+    EditText usernameEditText = findViewById(R.id.usernameEditText);
+    EditText passwordEditText = findViewById(R.id.passwordEditText);
+
+    if (loginModeActive) {
+
+      ParseUser.logInInBackground(usernameEditText.getText().toString(), passwordEditText.getText().toString(), new LogInCallback() {
+        @Override
+        public void done(ParseUser user, ParseException e) {
+          if(e == null){
+            Log.i("Info", "User Is Logged In");
+
+            redirectIfLoggedIn();
+
+          }else {
+
+            String message = e.getMessage();
+
+            if(message.toLowerCase().contains("java")){
+
+              message =  e.getMessage().substring(e.getMessage().indexOf(" "));
+            }
+
+            Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+          }
+
+        }
+      });
+
+    } else {
+
+
+
+      ParseUser user = new ParseUser();
+
+      user.setUsername(usernameEditText.getText().toString());
+
+      user.setPassword(passwordEditText.getText().toString());
+
+      user.signUpInBackground(new SignUpCallback() {
+        @Override
+        public void done(ParseException e) {
+          if (e == null) {
+            Log.i("Info", " Signed Up");
+
+            redirectIfLoggedIn();
+
+          } else {
+
+            String message = e.getMessage();  // for error message
+
+            if(message.toLowerCase().contains("java")){
+
+              message =  e.getMessage().substring(e.getMessage().indexOf(" "));
+            }
+
+            Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();  // short error message
+          }
+
+        }
+      });
+    }
+  }
+
+
+
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    setTitle("WhatsApp Login");
+
+    redirectIfLoggedIn();
+
+    
+    ParseAnalytics.trackAppOpenedInBackground(getIntent());
+  }
+
+}
